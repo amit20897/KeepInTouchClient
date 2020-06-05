ff7b5f1346bf48707be677724c9884968fe403e3
Amit <amit20897@gmail.com>
2020-06-06 02:13:49 +0300

    push notifciations

--- App.js
+++ App.js
@@ -1,16 +1,63 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Platform, StatusBar, StyleSheet, View, Vibration } from 'react-native';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
 
 import useCachedResources from './hooks/useCachedResources';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import LinkingConfiguration from './navigation/LinkingConfiguration';
+import PrioritySelectionScreen from './screens/PrioritySelectionScreen'
 
 const Stack = createStackNavigator();
 
 export default function App(props) {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState({});
+
   const isLoadingComplete = useCachedResources();
+
+  async function registerForPushNotificationsAsync () {
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        console.log('Failed to get push token for push notification!');
+        return;
+      }
+      token = await Notifications.getExpoPushTokenAsync();
+      console.log(token);
+      setExpoPushToken(token);
+    } else {
+      console.log('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+      Notifications.createChannelAndroidAsync('default', {
+        name: 'default',
+        sound: true,
+        priority: 'max',
+        vibrate: [0, 250, 250, 250],
+      });
+    }
+  }
+
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+    Notifications.addListener(_handleNotification);
+  }, []);
+
+  function _handleNotification(notification) {
+    Vibration.vibrate();
+    console.log(notification);
+    setNotification(notification);
+  }
 
   if (!isLoadingComplete) {
     return null;
@@ -21,6 +68,7 @@ export default function App(props) {
         <NavigationContainer linking={LinkingConfiguration}>
           <Stack.Navigator>
             <Stack.Screen name="Root" component={BottomTabNavigator} />
+            <Stack.Screen name="PrioritySelectionScreen" component={PrioritySelectionScreen} />
           </Stack.Navigator>
         </NavigationContainer>
       </View>
